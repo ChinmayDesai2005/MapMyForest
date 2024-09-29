56c2e87398c9ac6f49db952c403c11f65a518f5b
ShaanVeer21 <shaanveersingh21@gmail.com>
2024-09-29 16:27:45 +0530

    HomePage

---
+++ client/src/components/GetStartedButton/GetStartedButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './GetStartedButton.css'
+
+function GetStartedButton() {
+  return (
+    <div className="getStartedParent">
+      <div className="getStarted">Get Started</div>
+    </div>
+  )
+}
+
+export default GetStartedButton
