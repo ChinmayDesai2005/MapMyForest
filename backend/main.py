6239d2079c4dc60dbd61899715dc58cac5174955
Gautam-04 <2022.gautam.rai@ves.ac.in>
2024-10-15 20:08:57 +0530

    Done with most  of the backend
    Signed-off-by: Gautam-04 <2022.gautam.rai@ves.ac.in>

--- backend/main.py
+++ backend/main.py
@@ -10,8 +10,8 @@ PORT = os.environ['PORT']
 
 # Blueprints
 from blueprints.treeEnum.treeEnum import treeEnum
-from routes.user_routes import register_user,login_user
-from routes.project_routes import create_project
+from routes.user_routes import register_user,login_user,create_new_or_update_analysis
+from routes.project_routes import create_project, access_all_project, find_one_project_and_update, add_or_update_image, add_new_video
 
 app = Flask(__name__)
 CORS(app)
@@ -20,7 +20,12 @@ CORS(app)
 app.register_blueprint(treeEnum)
 app.register_blueprint(register_user)
 app.register_blueprint(login_user)
+app.register_blueprint(create_new_or_update_analysis)
 app.register_blueprint(create_project)
+app.register_blueprint(access_all_project)
+app.register_blueprint(find_one_project_and_update)
+app.register_blueprint(add_or_update_image)
+app.register_blueprint(add_new_video)
 
 if __name__ == "__main__":
     app.run(port=PORT, debug=True)
--- backend/routes/user_routes.py
+++ backend/routes/user_routes.py
@@ -1,8 +1,11 @@
 from flask import Blueprint
 from controller.user_controller import registerUser,loginUser
+from controller.analysis_controller import CreateNewOrUpdateAnalysis
+from middlewares.auth_middleware import authenticate
 
 register_user = Blueprint('register_user',__name__)
 login_user = Blueprint('login_user',__name__)
+create_new_or_update_analysis = Blueprint('create_new_or_update_analysis',__name__)
 
 @register_user.route('/api/v1/user/register',methods=['POST'])
 def register():
@@ -11,3 +14,8 @@ def register():
 @login_user.route('/api/v1/user/login',methods=['POST'])
 def login(): 
     return loginUser()
+
+@create_new_or_update_analysis.route('/api/v1/project/analysis',methods=['POST'])
+@authenticate
+def CreateOrUpdateAnalysis(): 
+    return CreateNewOrUpdateAnalysis()
