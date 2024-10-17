71e5d53442dc77eb9f36169dd0b4a0252c967b16
Gautam-04 <2022.gautam.rai@ves.ac.in>
2024-10-17 22:24:39 +0530

    Updated frontend
    Signed-off-by: Gautam-04 <2022.gautam.rai@ves.ac.in>

--- client/src/components/ProjectHeader/ProjectHeader.jsx
+++ client/src/components/ProjectHeader/ProjectHeader.jsx
@@ -16,7 +16,7 @@ function ProjectHeader() {
     <header className="projectheader-container">
       <nav className="nav-links">
         <a href="/" className="nav-link">Home</a>
-        <a href="/projects" className="nav-link">Projects</a>
+        <a href="/project" className="nav-link">Projects</a>
         <a href="/about" className="nav-link">About</a>
         <a href="/contact" className="nav-link">Contact</a>
       </nav>
