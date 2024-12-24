aded21c53d7fa769b716e6d84bd32770f7a1fa2b
Gautam-04 <2022.gautam.rai@ves.ac.in>
2024-12-24 16:48:32 +0530

    updated the controller for status controller
    Signed-off-by: Gautam-04 <2022.gautam.rai@ves.ac.in>

--- backend/DBmodels/projects_model.py
+++ backend/DBmodels/projects_model.py
@@ -13,12 +13,15 @@ db = client.get_database('mapmyforest')
 project_collection = db.get_collection('projects')
 
 project_collection.create_index('project_name',unique=True)
-
+# ["Incomplete", "In Progress", "Completed"]
 class Project:
-    def __init__(self, project_name, location, user_id,tree_images=None, videoURL=""):
+    def __init__(self, project_name, location , jurisdiction, currentStatus ,user_id, tree_images=None, tree_types=None, videoURL=""):
         self.project_name = project_name
         self.location = location
+        self.jurisdiction = jurisdiction
+        self.currentStatus = currentStatus
         self.tree_images = tree_images if tree_images else []
+        self.tree_types = tree_types if tree_types else []
         self.annotated_images = []
         self.videoURL= videoURL
         self.created_at = datetime.now()
@@ -36,8 +39,11 @@ class Project:
         return {
             'project_name': self.project_name,
             'location': self.location,
+            'jurisdiction': self.jurisdiction,
+            'currentStatus': self.currentStatus,
             'created_at': self.created_at.isoformat(),
             'tree_images': self.tree_images,
+            'tree_types': self.tree_types,
             'annotated_images': self.annotated_images,
             'videoURL': self.videoURL,
             'user_id': self.user_id 
